a5b44632d2e8c46274324d6d36c5872d805ceefb
tantrungse <tantrungse@gmail.com>
2023-08-11 11:30:35 +0700

    Setting up the models

--- LitteLemonAPI/admin.py
+++ LitteLemonAPI/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Category, MenuItem, Order, OrderItem, Cart
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(MenuItem)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Cart)
