f2fcf40a2ac4c0ec9fb2242c77d1a4ea2dbfe072
tantrungse <tantrungse@gmail.com>
2023-08-11 16:19:53 +0700

    Setting up user registration and token generation endpoints

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'LitteLemonAPI',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +132,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DJOSER = {
+    "USER_ID_FIELD": "username"
+}
